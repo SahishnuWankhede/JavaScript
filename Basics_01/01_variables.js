82ea97f32ac2c65bfa237a97f35d6fbc87b945b3
Sahishnu Wankhede <89682568+SahishnuWankhede@users.noreply.github.com>
2025-10-25 18:16:57 +0000

    variables

---
+++ Basics_01/01_variables.js
@@ -0,0 +1,29 @@
+// const accountId = 23322
+// let accountEmail = "sahishnu.wankhede@gmial.com"
+// var accountPassword = "12345"
+// accountCity = "Jaipur"
+
+// // accountID = 3; not allowed in constant
+
+// console.log(accountId);
+// accountEmail="rashmi.wankhede"
+// console.log(accountEmail);
+// // in Let the seconed input is considered
+// accountPassword = "1111222";
+// console.log(accountPassword);
+// // in var also the seconed input will be considered
+// accountCity = "Mumbai"
+// console.log(accountCity);
+// // if we dont initialize the const, let and var and then we re-initialze the same input 2nd time, then the 
+
+// Practice 1
+
+const accountID = 1321;
+let accountName = 'Sahishnu';
+let accountSName;
+let name = null
+
+console.table([accountID, accountName, accountSName, name]);
+console.log(typeof "Sahishnu");
+console.log(null);
+console.log(undefined);
