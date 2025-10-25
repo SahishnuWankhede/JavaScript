95181239c35b7eaf1a79be1b3844435f8c9b72a4
Sahishnu Wankhede <89682568+SahishnuWankhede@users.noreply.github.com>
2025-10-25 18:25:14 +0000

    Add detailed comments on JavaScript datatypes and ECMA standards

--- Basics_01/02.dataTypes.js
+++ Basics_01/02.dataTypes.js
@@ -1,2 +1,17 @@
 "use strict";
 
+/*
+    ECMA standards
+    datatypes in javascript
+        --> number => 2 power 53
+        --> bigInt
+        --> string => "....."
+        --> boolean => true/false
+        --> null => standalone value
+        --> undefined => when we dont defined value
+        --> symbol => unique
+        --> object
+
+        console.log(typeof "Sahishnu"); --> answer will be string
+        console.log(typeof null); --> answer will be object
+*/
