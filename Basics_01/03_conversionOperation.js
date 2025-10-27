ed36dc7c408e1ce4fa22996689d3b1db7f750346
Sahishnu Wankhede <89682568+SahishnuWankhede@users.noreply.github.com>
2025-10-27 15:07:05 +0000

    Implement conversion operations and string manipulations in JavaScript

---
+++ Basics_01/03_conversionOperation.js
@@ -0,0 +1,56 @@
+// let score = "33abc";
+let score = "33";
+// let score = null;
+// let score = undefined;
+// let score = "Sahishnu";
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// let IsbooleanValue = 1;
+let IsbooleanValue = 0;
+let booelanValue = Boolean(IsbooleanValue);
+// console.log(booelanValue);
+
+// conversion of string to number
+let value = "121112";
+let stringValue = Number(value);
+// console.log(stringValue);
+// console.log(typeof stringValue);
+
+//conversion of number to string
+let num = 33;
+let ValueNum = String(num);
+// console.log(ValueNum);
+// console.log(typeof ValueNum);
+
+// operations
+
+let val = 10;
+let negValue = -val;
+// console.log(negValue);
+
+let Str1 = "hello "
+let Str2 = "Sahishnu"
+
+let str3 = Str1+Str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+
+let x = 4;
+let y = 5;
+
+const z = x++;
+const b = y++;
+
+console.log(`x: ${x}, z:${z}`);
+console.log(`y: ${y}, b:${b}`);
