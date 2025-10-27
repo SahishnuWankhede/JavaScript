97d8771974ca49eac4bfea580ec89045b497edb5
Sahishnu Wankhede <89682568+SahishnuWankhede@users.noreply.github.com>
2025-10-27 15:45:21 +0000

    Add comparison examples and data type summary in JavaScript

---
+++ Basics_01/04_comparison.js
@@ -0,0 +1,10 @@
+//comparison in javascript
+
+let a = 12;
+let b = 2;
+let c;
+// console.log("Is a less than b?" +(a<b));
+
+console.log("2">1);
+console.log("02">1);
+
---
+++ Basics_01/test-summary.js
@@ -0,0 +1,23 @@
+console.log("Sahishnu Wankhede");
+
+/*
+    data types in javascript :-
+    primitive and non primitive
+
+    1) Primitive datatypes
+        a) String
+        b) Number
+        c) Boolean
+        d) null
+        e) undefined
+        f) Symbol
+        g) BigInt
+
+    2) Non primitive datatypes (refernce type)
+        a) object
+        b) Array
+        c) Functions
+
+
+    is JAvascript dynamically typed or statically type language
+*/
