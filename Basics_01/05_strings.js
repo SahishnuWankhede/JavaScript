9934a87496bfbeffd9ab9c4e9df4e801ad8382bc
Sahishnu Wankhede <89682568+SahishnuWankhede@users.noreply.github.com>
2025-11-01 07:10:17 +0000

    String Changes

---
+++ Basics_01/05_strings.js
@@ -0,0 +1,26 @@
+let name = "Sahishnu"
+let repoCount = 50;
+
+// console.log(`Hello, My name is ${name} and my repoCount is ${repoCount}`);
+
+
+const newName = new String("   Sahishnu Wankhede              ");
+
+console.log(newName[2]);
+console.log(newName[6]);
+
+console.log(newName.__proto__);
+
+// newName.toLowerCase();
+
+console.log(newName.toUpperCase());
+
+console.log(newName.charAt(3));
+console.log(newName);
+console.log(newName.trim());
+
+const url = "https://sahishnu.com/sahishnuWankhede";
+
+console.log(url.replace('Wankhede', 'wankhe'));
+
+console.log(url.includes('hitesh'));
