ea1b2b64262693c48c2c75f44e19fd883f3b1662
Sahishnu Wankhede <89682568+SahishnuWankhede@users.noreply.github.com>
2025-11-02 10:45:41 +0000

    Add date and timestamp examples in JavaScript

---
+++ Basics_01/07_datesinJS.js
@@ -0,0 +1,17 @@
+//dates 
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+
+let newDate = new Date(2024, 0, 23);
+// console.log(myDate.toDateString());
+
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+
+console.log(Math.floor(Date.now()/1000));
+console.log(newDate.getMonth());
