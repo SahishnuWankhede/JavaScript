902f13e85f2660a2c8d3cf06c17a9e8294c539d4
Sahishnu Wankhede <89682568+SahishnuWankhede@users.noreply.github.com>
2025-11-02 06:25:18 +0000

    Add initial implementation of number and math operations in JavaScript

---
+++ Basics_01/06_num_and_math.js
@@ -0,0 +1,25 @@
+// Number and Maths in JavaScript
+const score = 100;
+// console.log(score);
+
+
+const balance = new Number(1000);
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1));
+
+// ++++++++++++++++++++++++++++++++++  MATHS +++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-6));
+
+// console.log(Math.round(3.444));
+// console.log(Math.ceil(4.33));
+// console.log(Math.floor(3.5555));
+
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log((Math.random() * (max-min + 1)));
